2b301143c4a9e4f2ad772b7bb917b4b3747d9f5a
Jan Kasik <honza.kasik1@seznam.cz>
2015-12-08 15:37:35 +0100

    Fixed variable in main script

--- monitor.sh
+++ monitor.sh
@@ -6,7 +6,7 @@ GRAPH_FILE="usage.png"
 
 if [ -n "$PID" ] && ps -p "$PID" -o "pid=" >/dev/null 2>&1; then
     while true; do
-        ps --pid $1 -o pid=,size=,%cpu= >> $LOG_FILE
+        ps --pid $PID -o pid=,size=,%cpu= >> $LOG_FILE
         gnuplot -c gnuplot.script $LOG_FILE $GRAPH_FILE
         sleep 1
     done
