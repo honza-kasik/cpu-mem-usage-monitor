39c69fbc4e4a81545d531f62d19c8be2c574164f
Jan Kasik <jkasik@redhat.com>
2015-12-15 09:24:02 +0100

    Script now kills itself if process is killed, added time

--- monitor.sh
+++ monitor.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 PID=$1
+SLEEP_TIME=${2-1}
 LOG_FILE="usage.log"
 GRAPH_FILE="usage.png"
 
-if [ -n "$PID" ] && ps -p "$PID" -o "pid=" >/dev/null 2>&1; then
-    while true; do
-        ps --pid $PID -o pid=,size=,%cpu= >> $LOG_FILE
+if [ -n "$PID" ] && ps --pid "$PID" -o "pid=" >/dev/null 2>&1; then
+    while ps --pid $PID -o "pid=" >/dev/null 2>&1; do
+        ps --pid $PID -o pid=,size=,%cpu= | while IFS= read -r line;
+                                              do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line";
+                                            done >> $LOG_FILE
         gnuplot -c gnuplot.script $LOG_FILE $GRAPH_FILE
-        sleep 1
+        sleep $SLEEP_TIME
     done
 else
-    >&2 echo "You have to supply a PID of running process as first argument!";
+    >&2 echo "ERROR: You have to supply a PID of running process as first argument!";
 fi
