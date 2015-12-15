8b1c866f7a24e68a671a53ae9a975a6b9dab02cb
Jan Kasik <jkasik@redhat.com>
2015-12-15 09:56:06 +0100

    Added flag arguments

--- monitor.sh
+++ monitor.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
-
-PID=$1
-SLEEP_TIME=${2-1}
+GRAPH=false
+SLEEP_TIME=1
 LOG_FILE="usage.log"
 GRAPH_FILE="usage.png"
 
-if [ -n "$PID" ] && ps --pid "$PID" -o "pid=" >/dev/null 2>&1; then
+while getopts 'p:gl:d:s' flag; do
+  case "${flag}" in
+    p) PID="${OPTARG}" ;;
+    g) GRAPH=true ;;
+    l) LOG_FILE="${OPTARG}" ;;
+    d) GRAPH_FILE="${OPTARG}" ;;
+    s) SLEEP_TIME="${OPTARG}" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
+
+if [ -n "$PID" ] && ps --pid $PID -o "pid=" >/dev/null 2>&1; then
     while ps --pid $PID -o "pid=" >/dev/null 2>&1; do
         ps --pid $PID -o pid=,size=,%cpu= | while IFS= read -r line;
                                               do printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$line";
                                             done >> $LOG_FILE
-        gnuplot -c gnuplot.script $LOG_FILE $GRAPH_FILE
+        if [[ "$GRAPH" = true ]]; then
+          gnuplot -c gnuplot.script $LOG_FILE $GRAPH_FILE
+        fi
         sleep $SLEEP_TIME
     done
 else
-    >&2 echo "ERROR: You have to supply a PID of running process as first argument!";
+    >&2 echo "ERROR: You have to supply a PID of running process!";
 fi
